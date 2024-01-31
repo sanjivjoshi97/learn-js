eb3c060d9ec126fe3de4abcf472cf00fd144ee7c
Sanjiv Joshi <joshi.sanj@northeastern.edu>
2024-01-31 16:15:11 -0500

    Practice arrays

--- quiz/scripts/array1-q.js
+++ quiz/scripts/array1-q.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     const list = document.getElementById('Top5');
     const inp = document.getElementById('inp');
     const btn = document.getElementById('btn');
@@ -8,25 +8,30 @@ window.onload = function() {
     const MAX_HISTORY = 5;
 
     btn.onclick = () => {
-    // we will only allow a term to be entered if the search input isn't empty
-      if (inp.value !== '') {
-              // empty the list so that we don't display duplicate entries
-              // the display is regenerated every time a search term is entered.
-              list.innerHTML = '';
+        // we will only allow a term to be entered if the search input isn't empty
+        if (inp.value !== '') {
+            myHistory.unshift(inp.value);
+            // empty the list so that we don't display duplicate entries
+            // the display is regenerated every time a search term is entered.
+            list.innerHTML = '';
 
-              // loop through the sorted array, and display all the search terms in the list
-              for (const itemText of myHistoryCopy) {
-                
-              }
+            let myHistoryCopy = Array.from(myHistory);
+            myHistoryCopy.sort((a, b) => a.length - b.length);
+            // loop through the sorted array, and display all the search terms in the list
+            for (const itemText of myHistoryCopy) {
+                let item = document.createElement("li");
+                item.innerHTML = itemText;
+                list.appendChild(item);
+            }
 
-              // If the array length is 5 or more, remove the oldest search term
-              if (myHistory.length >= MAX_HISTORY) {
-                
-              }
+            // If the array length is 5 or more, remove the oldest search term
+            if (myHistory.length >= MAX_HISTORY) {
+                myHistory.pop();
+            }
 
-              // empty the search input and focus it, ready for the next term to be entered
-              inp.value = '';
-              btn.focus();
-          }
+            // empty the search input and focus it, ready for the next term to be entered
+            inp.value = '';
+            btn.focus();
+        }
     }
 }
