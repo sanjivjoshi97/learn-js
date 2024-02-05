9bf27b6d19c06c03792ad46bf43bc7535649fc3f
Sanjiv Joshi <joshi.sanj@northeastern.edu>
2024-02-05 15:02:24 -0500

    fix map-deep.js

--- quiz/scripts/map-deep.js
+++ quiz/scripts/map-deep.js
@@ -3,16 +3,16 @@ const originalMatrix = [
     [3, 2, 1],
     [4, 5, 6],
     [7, 9, 8]
-  ];
-  
-  const newMatrix = originalMatrix.map((row) => {
-    return row.sort();
-  });
-  
-  // Modifying the original matrix (changing the last element of the first row)
-  originalMatrix[0][2] = 99;
-  
-  // Displaying the original and new matrices
-  console.log('Original Matrix (modified):', originalMatrix);
-  console.log('New Matrix (unaffected):', newMatrix);
+];
+
+const newMatrix = originalMatrix.map((row) => {
+    return [...row].sort();
+});
+
+// Modifying the original matrix (changing the last element of the first row)
+originalMatrix[0][2] = 99;
+
+// Displaying the original and new matrices
+console.log('Original Matrix (modified):', originalMatrix);
+console.log('New Matrix (unaffected):', newMatrix);
   
